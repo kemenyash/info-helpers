6d8e3fdcd12587e8c3cff8abe612c740b9b835cf
Vladislav Kemenyash <75218089+kemenyash@users.noreply.github.com>
2022-05-07 19:19:08 +0300

    Update change_root.sql

--- MySQL/change_root.sql
+++ MySQL/change_root.sql
@@ -2,4 +2,3 @@ UPDATE mysql.user SET authentication_string=null WHERE User='root';
 flush privileges;
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '@PASSWORD';
 flush privileges;
-exit;
